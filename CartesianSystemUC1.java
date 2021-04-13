46a70b30550d98216f0869e07417622201a1ce66
Nivedita-PA <npal4661@gmail.com>
2021-04-14 01:43:13 +0530

    [Nivedita]: added the length of line calculation for given coordinates

---
+++ CartesianSystemUC1.java
@@ -0,0 +1,17 @@
+
+public class CartesianSystemUC1 {
+
+	public static void main(String[] args) {
+	     
+		int x1 = 4;
+		int y1 = 2;
+		int x2 = 8;
+		int y2 = 4;
+		
+		double length_Line = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)); 
+		System.out.println(length_Line);
+		
+		
+	}
+
+}
